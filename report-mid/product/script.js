19b0ea5d6c67de4a76d0a36a6d074215da4b3427
Oporu <a0908017366@gmail.com>
2022-10-31 11:42:03 +0800

    modified:   report-mid/product/script.js

--- report-mid/product/script.js
+++ report-mid/product/script.js
@@ -1,3 +1,1 @@
-function rickroll(){
-	window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ",target="_blank")
-}
+function rickroll(){window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ",target="_blank")}
