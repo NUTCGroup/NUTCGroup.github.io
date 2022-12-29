aadfe7eeefdfbde7da9894d812762ac8fcb511b5
Oporu <a0908017366@gmail.com>
2022-12-29 17:33:19 +0800

    better

--- product/itemModal.js
+++ product/itemModal.js
@@ -20,7 +20,7 @@ fetch("modal.html").then(r=>r.text()).then(m=>{
 	//讀好檔案會被執行 m 為檔案內容 string
 	//console.log(m); not formatted
 	//eval(`\`${m}\``); // formatted
-	d = document.documentElement.getElementsByTagName("body")[0]
+	var d = document.documentElement.getElementsByTagName("body")[0]
 	product.forEach(p=>{
 		d.innerHTML += (eval(`\`${m}\``));
 	});
