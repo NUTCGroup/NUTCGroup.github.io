5f81e74c5da226da67e65c4fe2d20e97f9bd8727
スカネモエロ <93065841+sukanemoero@users.noreply.github.com>
2022-12-29 14:47:52 +0800

    a

--- product/itemModal.js
+++ product/itemModal.js
@@ -1,5 +1,6 @@
 a = 0
 fetch("modal.html").then(r=>r.text()).then(m=>{
     //讀好檔案會被執行 m 為檔案內容 string
-    console.log(m);
+    //console.log(m); not formatted
+    eval(`\`${m}\``); // formatted
 })
