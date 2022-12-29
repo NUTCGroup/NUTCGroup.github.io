80c27ecfa73233ddfdff355011e2064cd98e819d
Oporu <a0908017366@gmail.com>
2022-12-29 16:51:01 +0800

    test

--- product/itemModal.js
+++ product/itemModal.js
@@ -1,6 +1,26 @@
-a = 0
+//"image" "title" "detail"
+product = [
+	{
+		"image": "",
+		"title": "title here",
+		"detail": "內容哈哈"
+	},
+	{
+		"image": "",
+		"title": "title here",
+		"detail": "內容哈哈"
+	},
+	{
+		"image": "",
+		"title": "title here",
+		"detail": "內容哈哈"
+	}
+]
 fetch("modal.html").then(r=>r.text()).then(m=>{
-    //讀好檔案會被執行 m 為檔案內容 string
-    //console.log(m); not formatted
-    eval(`\`${m}\``); // formatted
+	//讀好檔案會被執行 m 為檔案內容 string
+	//console.log(m); not formatted
+	//eval(`\`${m}\``); // formatted
+	product.forEach(p=>{
+		document.write(eval(`\`${m}\``));
+	});
 }) 
