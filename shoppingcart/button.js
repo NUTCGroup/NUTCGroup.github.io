acd3969a78166c1935ecfd31c3c8c8216c4848ca
スカネモエロ <93065841+sukanemoero@users.noreply.github.com>
2023-01-01 00:04:33 +0800

    eluo update

---
+++ shoppingcart/button.js
@@ -0,0 +1,19 @@
+countinput = [...document.getElementsByClassName("countinput")]
+for(i = 0;i < countinput.length;i++){
+    countinput[i].value = 0
+    countinput[i].max = 100
+    countinput[i].min = 0
+    countinput[i].style.textAlign= "right"
+    countinput[i].oninput = function(self){
+        value = parseInt(this.value)
+        if(value >= 100){
+            this.value = 100
+        }
+        else if(value < 100 && value >= 0){
+            this.value = parseInt(this.value)
+        }
+        else{
+            this.value = 0
+        }
+    }
+}
