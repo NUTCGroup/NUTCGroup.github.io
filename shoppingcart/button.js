572899a73514072fd51428b7a01fbbe0cfdc077b
スカネモエロ <93065841+sukanemoero@users.noreply.github.com>
2023-01-01 12:31:34 +0800

    eluo update

--- shoppingcart/button.js
+++ shoppingcart/button.js
@@ -15,5 +15,19 @@ for(i = 0;i < countinput.length;i++){
         else{
             this.value = 0
         }
+        update()
     }
+}
+
+itemCost = [40 , 45 , 45]
+
+function update(){
+    var cost = 0
+    var upd = document.querySelector("div#value-first span#value-contains")
+    upd.innerHTML = ""
+    for(i = 0;i < countinput.length;i++){
+        cost += countinput[i].value * itemCost[i]
+        upd.innerHTML += `${countinput[i].name}: ${countinput[i].value * itemCost[i]}<br>`
+    }
+    upd.innerHTML += cost
 }
