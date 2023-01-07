48d551a377e3f3aee71d1aca7aaefdfa26bb28e6
スカネモエロ <93065841+sukanemoero@users.noreply.github.com>
2023-01-07 16:08:15 +0800

    eluo update

--- js/shoppingCartOnNavBar.js
+++ js/shoppingCartOnNavBar.js
@@ -28,7 +28,9 @@ class shoppingCartOnNavBar{
 		//	<td class="align-middle">${amount}杯</td>
 		//	<td class="align-middle text-right">${price*amount}</td>
 		//  </tr>
-		if (!(product in [0,1,2])) return console.error("invaild product id");
+		if (!(product in [0,1,2])){
+			return console.error("invaild product id")
+		};
 
 		this.#holder.innerHTML += `<tr><td class='text-center'><i class='fa-solid fa-trash'></i></td><td class='align-middle'><img src='${products[product].image}' style='width:50px;'></td><td class='align-middle'>${products[product].name}</td><td class='align-middle'>1杯</td><td class='align-middle text-right'>$40</td></tr>`;
 		return;
--- productDescription/script.js
+++ productDescription/script.js
@@ -1,8 +1,45 @@
 id = Number(new URL(window.location.href).searchParams.get("product"));
+snd = document.getElementById("sender")
 if (Number.isInteger(id)){
 	id = id%products.length;
+	snd.value = id
 	product = products[id];
 }else product = products[0];
-[["title", "innerHTML", "name"],["content", "innerHTML", "description"],["price_", "innerHTML", "price"], ["item", "src", "image"]].forEach((e)=>{
+[
+	["title", "innerHTML", "name"],
+	["content", "innerHTML", "description"],
+	["price_", "innerHTML", "price"], 
+	["item", "src", "image"]].forEach((e)=>{
 	document.getElementById(e[0])[e[1]] = product[e[2]].toString();
 })
+
+inp = document.querySelector("div#input input")
+inp.oninput = function(){
+    value = parseInt(this.value)
+    if(value >= 1000){
+        this.value = 1000
+    }
+    else if(value < 1000 && value >= 0){
+        this.value = parseInt(this.value)
+    }
+    else{
+        this.value = 0
+    }
+}
+
+snd.onclick = function(){
+	if(snd.innerText == "nop"){
+		return
+	}
+	else if(inp.value == 0){
+		snd.innerText = "nop"
+		console.log(snd.innerText)	
+		setTimeout(function(){
+			snd.innerText = "添加到購物車"
+		},1000)
+	}
+	else{
+		shoppingCartOnNavBar.displayToCart(snd.value);
+		inp.value = 0
+	}
+}
